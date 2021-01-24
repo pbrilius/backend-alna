48912b79512d0bda165f5bb60127ce4af2de0734
Povilas <pbrilius@gmail.com>
2021-01-24 21:06:58 +0200

    php units passing.

--- tests/Feature/JobPostControllerTest.php
+++ tests/Feature/JobPostControllerTest.php
@@ -16,11 +16,18 @@ class JobPostControllerTest extends TestCase
     public function testJobPostController()
     {
         $response = $this->post('/crawl', [
-            'URL' => 'https://medium.com/swlh/fun-with-python-3-hacking-instagram-giveaways-35e5b1d51670',
-            'CSS3' => 'article.meteredContent',
+            [
+                'name' => 'URL',
+                'value' =>
+                    'https://medium.com/swlh/fun-with-python-3-hacking-instagram-giveaways-35e5b1d51670',
+            ],
+            [
+                'name' => 'CSS3',
+                'value' => 'article.meteredContent',
+            ],
         ]);
 
         $response->assertStatus(200);
-        $response->assertSeeText('Data inserted:');
+        $response->assertJsonStructure(['site', 'css3']);
     }
 }
